6a2a6b665aec0a26584a04ce158991f9d95eb30c
mziwonkekabuyaphi <fundiledolophu@gmail.com>
2026-07-08 23:53:31 +0200

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -8,7 +8,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const SUPABASE_URL = 'https://odpugxrihfspaucsdqjj.supabase.co';
 
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcHVneHJpaGZzcGF1Y3NkcWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTQ5MDcsImV4cCI6MjA5NDMzMDkwN30.c1xpL4p4llS8oNzdg0evCuJNaBMC1REPG0dNk47WeMU';
 
 // ===============================
 // 🚀 CREATE CLIENT
